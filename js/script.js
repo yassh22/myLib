2d1a800ca3c394fce2eecba15ef7c1e5b420d6ae
Yash Panchal <yashvardan@Dark-Empire.local>
2020-04-03 23:15:05 -0400

    feat: added contact, footer and nav menus

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+  $('.hamburger-menu').click(function(){
+    $('.navigation').toggleClass('change');
+  });
+  
   lightbox.option({
     'wrapAround': true
-  })
+  });
 
   $(window).scroll(function(){
     let position = $(this).scrollTop();
@@ -11,12 +15,12 @@ $(document).ready(function(){
     } else {
       $('.gallery').removeClass('change');
     }
-  })
+  });
 
   $('.writers-accordion').click(function(event){
     if (event.target.id.split('-')[0] === 'button') {
       $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg')
       $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg')
     }
-  })
+  });
 });
